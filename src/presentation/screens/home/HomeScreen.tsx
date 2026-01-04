651d67b011c052117b64de5344515f33e9d26a8f
David <davidponceg207@gmail.com>
2026-01-04 10:27:48 -0600

    Menu Item component

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,70 +1,72 @@
 import { ScrollView, Text, View } from 'react-native';
 import { globalStyles } from '../../../config/theme/theme';
 import { Title } from '../../components/ui/Title';
+import { MenuItem } from '../../components/ui/MenuItem';
+
+const animationMenuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+]
 
 export const menuItems = [
-    // 01-animationMenuItems
-    {
-      name: 'Animation 101',
-      icon: 'cube-outline',
-      component: 'Animation101Screen',
-    },
-    {
-      name: 'Animation 102',
-      icon: 'albums-outline',
-      component: 'Animation102Screen',
-    },
-  
-  
-    // 02-menuItems
-    {
-      name: 'Pull to refresh',
-      icon: 'refresh-outline',
-      component: 'PullToRefreshScreen',
-    },
-    {
-      name: 'Section List',
-      icon: 'list-outline',
-      component: 'CustomSectionListScreen',
-    },
-    {
-      name: 'Modal',
-      icon: 'copy-outline',
-      component: 'ModalScreen',
-    },
-    {
-      name: 'InfiniteScroll',
-      icon: 'download-outline',
-      component: 'InfiniteScrollScreen',
-    },
-    {
-      name: 'Slides',
-      icon: 'flower-outline',
-      component: 'SlidesScreen',
-    },
-    {
-      name: 'Themes',
-      icon: 'flask-outline',
-      component: 'ChangeThemeScreen',
-    },
-  
-    // 03- uiMenuItems
-    {
-      name: 'Switches',
-      icon: 'toggle-outline',
-      component: 'SwitchScreen',
-    },
-    {
-      name: 'Alerts',
-      icon: 'alert-circle-outline',
-      component: 'AlertScreen',
-    },
-    {
-      name: 'TextInputs',
-      icon: 'document-text-outline',
-      component: 'TextInputScreen',
-    },
+
+  {
+    name: 'Pull to refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreen',
+  },
+  {
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'CustomSectionListScreen',
+  },
+  {
+    name: 'Modal',
+    icon: 'copy-outline',
+    component: 'ModalScreen',
+  },
+  {
+    name: 'InfiniteScroll',
+    icon: 'download-outline',
+    component: 'InfiniteScrollScreen',
+  },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'SlidesScreen',
+  },
+  {
+    name: 'Themes',
+    icon: 'flask-outline',
+    component: 'ChangeThemeScreen',
+  },
 ];
+
+const uiMenuItems = [
+  {
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+  {
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreen',
+  },
+  {
+    name: 'TextInputs',
+    icon: 'document-text-outline',
+    component: 'TextInputScreen',
+  },
+]
 
 export const HomeScreen = () => {
     return (
@@ -72,9 +74,37 @@ export const HomeScreen = () => {
             <View style={globalStyles.globalMargin}>
                 <ScrollView>
                     <Title text='Options' safe />
+
                     {
-                        menuItems.map(item => (
-                            <Text key={ item.component }>{item.name}</Text>
+                        animationMenuItems.map((item, index) => (
+                            <MenuItem
+                              key={ item.component }
+                              {...item}
+                              isFirst={ index === 0 }
+                              isLast={ index === animationMenuItems.length - 1 }
+                            />
+                        ))
+                    }
+                    <View style={{marginTop: 30}} />
+                    {
+                        menuItems.map((item, index) => (
+                            <MenuItem
+                              key={ item.component }
+                              {...item}
+                              isFirst={ index === 0 }
+                              isLast={ index === menuItems.length - 1 }
+                            />
+                        ))
+                    }
+                    <View style={{marginTop: 30}} />
+                    {
+                        uiMenuItems.map((item, index) => (
+                            <MenuItem
+                              key={ item.component }
+                              {...item}
+                              isFirst={ index === 0 }
+                              isLast={ index === uiMenuItems.length - 1 }
+                            />
                         ))
                     }
                 </ScrollView>
